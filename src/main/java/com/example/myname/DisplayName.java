e971cf48967947d0d856b06d4d1958e30cefb77f
Manideep1901 <manideepdakuri01@gmail.com>
2025-09-23 12:10:13 +0530

    Initial commit

---
+++ src/main/java/com/example/myname/DisplayName.java
@@ -0,0 +1,10 @@
+package com.example.myname;
+
+public class DisplayName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Manideep D");
+	}
+
+}
