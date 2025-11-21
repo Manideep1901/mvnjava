133396d814234d7a8bacdb68bc59ded851ff6afc
Manideep1901 <manideepdakuri01@gmail.com>
2025-11-21 06:09:27 +0530

    Update DisplayName.java

--- src/main/java/com/example/myname/DisplayName.java
+++ src/main/java/com/example/myname/DisplayName.java
@@ -4,7 +4,7 @@ public class DisplayName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My name is Manideep D");
+		System.out.println("My name is Manideep ");
 	}
 
 }
